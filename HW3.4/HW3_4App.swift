d46165dc4d6634d4fb8654c5c9d24dcd3f2b58fa
ageres7-dev <66694244+ageres7-dev@users.noreply.github.com>
2020-12-21 23:10:58 +0500

    Initial Commit

---
+++ HW3.4/HW3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_4App.swift
+//  HW3.4
+//
+//  Created by Сергей on 21.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct HW3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
